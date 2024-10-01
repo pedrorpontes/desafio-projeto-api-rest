a3b7803a32c9cc3727321312fdb18de0a96dcaa4
Pedro Pontes <72471440+pedrorpontes@users.noreply.github.com>
2024-10-01 00:10:37 -0300

    Bufix CORS

--- src/main/java/me/dio/DesafioProjetoApiRestApplication.java
+++ src/main/java/me/dio/DesafioProjetoApiRestApplication.java
@@ -1,13 +1,16 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class DesafioProjetoApiRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioProjetoApiRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioProjetoApiRestApplication.class, args);
+    }
 
 }
